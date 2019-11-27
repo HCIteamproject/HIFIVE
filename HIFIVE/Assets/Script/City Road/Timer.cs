f6fe64075b06337d5e16081f8bde5c3b3e9c2122
ezzanzzan <cjsdmswjd821@gmail.com>
2019-11-27 11:02:13 +0900

    v.1.0.0

---
+++ HIFIVE/Assets/Script/City Road/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float time;
+    public Text Timer_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        Timer_text.text = string.Format("{00:00:00}", time);
+    }
+}
