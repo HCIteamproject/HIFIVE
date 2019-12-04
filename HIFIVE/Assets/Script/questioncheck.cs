9fb6e341d3d43ffe28fb9454ceea959dbb6d60bc
ezzanzzan <cjsdmswjd821@gmail.com>
2019-12-04 13:11:34 +0900

    v.1.0.2

---
+++ HIFIVE/Assets/Script/questioncheck.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class questioncheck : MonoBehaviour
+{
+    public string check1 = "1";
+    public string check2 = "2";
+    public string check3 = "3";
+    public void questionCheck1()
+    {
+        if (commonquestion.check.Equals(check1))
+        {
+            GameObject.Find("GameScreen").transform.FindChild("o").gameObject.SetActive(true);
+            GameObject.Find("GameScreen").transform.FindChild("Panel").gameObject.SetActive(false);
+            int num = Random.Range(0, 30);
+            commonquestion.setnum(num);
+        }
+        else
+        {
+            PlayerStats.Instance.TakeDamage();
+            GameObject.Find("GameScreen").transform.FindChild("x").gameObject.SetActive(true);
+            GameObject.Find("GameScreen").transform.FindChild("Panel").gameObject.SetActive(false);
+            int num = Random.Range(0, 30);
+            commonquestion.setnum(num);
+
+        }
+    }
+
+    public void questionCheck2()
+    {
+        if (commonquestion.check.Equals(check2))
+        {
+            GameObject.Find("GameScreen").transform.FindChild("o").gameObject.SetActive(true);
+            GameObject.Find("GameScreen").transform.FindChild("Panel").gameObject.SetActive(false);
+            int num = Random.Range(0, 30);
+            commonquestion.setnum(num);
+        }
+        else
+        {
+            PlayerStats.Instance.TakeDamage();
+            GameObject.Find("GameScreen").transform.FindChild("o").gameObject.SetActive(true);
+            GameObject.Find("GameScreen").transform.FindChild("Panel").gameObject.SetActive(false);
+            int num = Random.Range(0, 30);
+            commonquestion.setnum(num);
+
+        }
+    }
+
+    public void questionCheck3()
+    {
+        if (commonquestion.check.Equals(check3))
+        {
+            GameObject.Find("GameScreen").transform.FindChild("o").gameObject.SetActive(true);
+            GameObject.Find("GameScreen").transform.FindChild("Panel").gameObject.SetActive(false);
+            int num = Random.Range(0, 30);
+            commonquestion.setnum(num);
+
+        }
+        else
+        {
+            PlayerStats.Instance.TakeDamage();
+            GameObject.Find("GameScreen").transform.FindChild("o").gameObject.SetActive(true);
+            GameObject.Find("GameScreen").transform.FindChild("Panel").gameObject.SetActive(false);
+            int num = Random.Range(0, 30);
+            commonquestion.setnum(num);
+
+        }
+    }
+}
---
+++ HIFIVE/Assets/unity-chan!/Unity-chan! Model/Scripts/CameraController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityChan
+{
+	enum MouseButtonDown
+	{
+		MBD_LEFT = 0,
+		MBD_RIGHT,
+		MBD_MIDDLE,
+	};
+
+	public class CameraController : MonoBehaviour
+	{
+		[SerializeField]
+		private Vector3 focus = Vector3.zero;
+		[SerializeField]
+		private GameObject focusObj = null;
+
+		public bool showInstWindow = true;
+
+		private Vector3 oldPos;
+
+		void setupFocusObject(string name)
+		{
+			GameObject obj = this.focusObj = new GameObject(name);
+			obj.transform.position = this.focus;
+			obj.transform.LookAt(this.transform.position);
+
+			return;
+		}
+
+		void Start ()
+		{
+			if (this.focusObj == null)
+				this.setupFocusObject("CameraFocusObject");
+
+			Transform trans = this.transform;
+			transform.parent = this.focusObj.transform;
+
+			trans.LookAt(this.focus);
+
+			return;
+		}
+	
+		void cameraTranslate(Vector3 vec)
+		{
+			Transform focusTrans = this.focusObj.transform;
+
+			vec.x *= -1;
+
+			focusTrans.Translate(Vector3.right * vec.x);
+			focusTrans.Translate(Vector3.up * vec.y);
+
+			this.focus = focusTrans.position;
+
+			return;
+		}
+
+		public void cameraRotate(Vector3 eulerAngle)
+		{
+			//Use Quaternion to prevent rotation flips on XY plane
+			Quaternion q = Quaternion.identity;
+ 
+			Transform focusTrans = this.focusObj.transform;
+			focusTrans.localEulerAngles = focusTrans.localEulerAngles + eulerAngle;
+
+			//Change this.transform.LookAt(this.focus) to q.SetLookRotation(this.focus)
+			q.SetLookRotation (this.focus) ;
+
+			return;
+		}
+	}
+}
